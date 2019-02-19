07c57fc1862f5fd5bb49e4b6c0f7fa349d23261f
Osmar <osmarmanrique20@gmail.com>
2019-02-19 11:21:01 -0800

    Mis practicas scala

---
+++ Unidad 1/Examen.scala
@@ -0,0 +1,68 @@
+//Numero par
+def listEvens(list:List[Int]): String ={
+    for(n <- list){
+        if(n%2==0){
+            println(s"$n is even")
+        }else{
+            println(s"$n is odd")
+        }
+    }
+    return "Done"
+}
+val l = List(1,2,3,4,5,6,7,8)
+val l2 = List(4,3,22,55,7,8)
+listEvens(l)
+listEvens(l2)
+
+//Afortunado numero 7
+def afortunado(list:List[Int]): Int={
+    var res=0
+    for(n <- list){
+        if(n==7){
+            res = res + 14
+        }else{
+            res = res + n
+        }
+    }
+    return res
+}
+
+val af= List(1,7,7)
+println(afortunado(af))
+
+//¿Puedes equilibrar?
+def balance(list:List[Int]): Boolean={
+    var primera = 0
+    var segunda = 0
+
+    segunda = list.sum
+
+    for(i <- Range(0,list.length)){
+        primera = primera + list(i)
+        segunda = segunda - list(i)
+
+        if(primera == segunda){
+            return true
+        }
+    }
+    return false
+}
+
+val bl = List(3,2,1)
+val bl2 = List(2,3,3,2)
+val bl3 = List(10,30,90)
+
+balance(bl)
+balance(bl2)
+balance(bl3)
+//Palindromo
+var pali="ana"
+var CheckPali= pali.reverse
+if(CheckPali==pali)
+{
+println("ana si es palindromo")
+}
+else
+{
+println("ana no es palindromo")
+}
